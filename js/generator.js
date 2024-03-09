de484efdd2c2e5d811f73a48bb9e4af9440af4ba
frdgseb <danildolgov735@gmail.com>
2024-03-09 22:41:56 +0500

    version 1

---
+++ js/generator.js
@@ -0,0 +1,50 @@
+const generation = document.getElementById('generation');
+const result = document.getElementById('result');
+const lengthPassword = document.getElementById('lengthPassword');
+
+//slider 
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+function getPassword(min, max) {
+    let random = Math.floor(Math.random() * (max - min + 1) + min);
+
+    let ArrayOfString = random.toString(36).split();
+
+    if (ArrayOfString.join('').length < output.innerHTML) {
+        return ArrayOfString.join('') + '6';
+    }
+
+    return ArrayOfString.join('');
+
+}
+
+generation.onclick = function() {
+    if (output.innerHTML == 12) {
+        result.textContent = getPassword(1e17, 1e18);
+    } else if (output.innerHTML == 11) {
+        result.textContent = getPassword(1e15, 1e16);
+    } else if (output.innerHTML == 10) {
+        result.textContent = getPassword(1e13, 1e14);
+    } else if (output.innerHTML == 9) {
+        result.textContent = getPassword(1e12, 1e13);
+    } else if (output.innerHTML == 8) {
+        result.textContent = getPassword(1e11, 1e12);
+    } else if (output.innerHTML == 7) {
+        result.textContent = getPassword(1e9, 1e10);
+    } else if (output.innerHTML == 6) {
+        result.textContent = getPassword(1e8, 1e9);
+    } else if (output.innerHTML == 5) {
+        result.textContent = getPassword(1e6, 1e7);
+    } else if (output.innerHTML == 4) {
+        result.textContent = getPassword(1e5, 1e6);
+    } else if (output.innerHTML == 3) {
+        result.textContent = getPassword(1e3, 1e4);
+    }
+}
